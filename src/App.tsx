2fc62b105dfa8113d32f2e54bdfb9a5be4578d51
Dann1y <leedanny0102@gmail.com>
2024-04-10 14:55:01 +0900

    chore: update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
@@ -77,8 +70,8 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            Edit sfdasd<Text style={styles.highlight}>App.tsx</Text> to change
+            this screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
